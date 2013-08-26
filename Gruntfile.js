0f2030367027d731286adff276f966fe309b47a1
Jason Aller <jraller@gmail.com>
2013-08-25 17:48:15 -0700

    initial framework

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        // Task configuration.
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                unused: true,
+                boss: true,
+                eqnull: true,
+                globals: {}
+            },
+            gruntfile: {
+                src: 'Gruntfile.js'
+            },
+            lib_test: {
+                src: ['lib/**/*.js', 'test/**/*.js']
+            }
+        },
+        nodeunit: {
+            files: ['test/**/*_test.js']
+        },
+        watch: {
+            gruntfile: {
+                files: '<%= jshint.gruntfile.src %>',
+                tasks: ['jshint:gruntfile']
+            },
+            lib_test: {
+                files: '<%= jshint.libTest.src %>',
+                tasks: ['jshint:libTest', 'nodeunit']
+            }
+        }
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task.
+    grunt.registerTask('default', ['jshint', 'nodeunit']);
+
+};
--- README.md
+++ README.md
@@ -2,3 +2,8 @@ Grunt-Cascade-Examples
 ======================
 
 a set of examples and test code for accessing Cascade Server Web Services in NodeJS
+
+Clone this repository to your workstation.
+Edit Gruntfile.js and modify the url for your TEST Cascade Server - do not use this with your production instance.
+Review the code - here you will find examples of each of the web service calls you might make with documentation and notes about quirks for each call.
+Run the tests - will document as they are written.
