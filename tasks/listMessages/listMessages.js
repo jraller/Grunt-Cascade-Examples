27c279cb651b28ad3a380d00b733df7be02732a3
Jason Aller <jraller@gmail.com>
2013-09-07 20:19:56 -0700

    rename listMessage

--- tasks/listMessages/listMessages.js
+++ tasks/listMessages/listMessages.js
@@ -108,7 +108,7 @@ function bugUser() {
 }
 
 module.exports = function (gruntObj) {
-	gruntObj.registerTask('listmessage', 'call the read action for the default connector container', function () {
+	gruntObj.registerTask('listmessages', 'call the read action for the default connector container', function () {
 		grunt = gruntObj;
 		done = this.async();
 		next([
